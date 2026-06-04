b0d28f063800b823dab51f614c0b8c515b4700a5
Yeshvikaa <yeshuprojects@gmail.com>
2026-06-04 11:47:05 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.get('/', (req, res) => {
 
 // Port and MongoDB setup
 const PORT = process.env.PORT || 5000;
-const MONGODB_URI = process.env.MONGODB_URI || 'MONGODB_URI=mongodb+srv://yaduraj:yaduraj@cluster0.vsk89qe.mongodb.net/yaduraj?retryWrites=true&w=majority&appName=Cluster0';
+const MONGODB_URI = process.env.MONGODB_URI || 'https://xephora.onrender.com';
 
 mongoose.connect(MONGODB_URI)
   .then(() => {
